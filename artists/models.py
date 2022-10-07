e8e17777c632fabc40144dd5b2a0a88ba156e4f5
AbdullahHafez <abdullah.alaa12@gmail.com>
2022-10-07 20:09:13 +0200

    fix: Fex models fields and add string descriptions

--- artists/models.py
+++ artists/models.py
@@ -6,3 +6,9 @@ from django.db import models
 class Artist(models.Model):
     stage_name = models.CharField(max_length=30, unique=True, blank=False, null=False)
     social_link = models.URLField(max_length=200, blank=True, null=False)
+
+    class Meta:
+        ordering = ['stage_name']
+
+    def __str__(self):
+        return self.stage_name
