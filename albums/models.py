a34f6d72cc9c07b9ae8f8b8ec7d16fa749b43bb9
AbdullahHafez <abdullah.alaa12@gmail.com>
2022-10-07 06:21:09 +0200

    feat: Create albums app & Album model

---
+++ albums/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+from artists.models import Artist
+
+
+class Album(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=30, default="New Album")
+    creation_date = models.DateTimeField(default=timezone.now)
+    release_date = models.DateTimeField(blank=False, null=False)
+    cost = models.DecimalField(decimal_places=2, blank=False, null=False)
