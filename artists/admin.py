52444512c1467cf7a2a18364c9828880a87e77f9
AbdullahHafez <abdullah.alaa12@gmail.com>
2022-10-13 23:41:16 +0200

    feat: Add number of approved albums to artists list and allow artist to add albums from its form

--- artists/admin.py
+++ artists/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Artist
+from albums.models import Album
 
 # Register your models here.
+
+@admin.display(description='# Approved Albums')
+def approved_albums_count(obj):
+    return obj.album_set.filter(is_approved=True).count()
+
+
+class AlbumInline(admin.TabularInline):
+    model = Album
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('stage_name', approved_albums_count)
+    inlines = [AlbumInline, ]
+
+
+admin.site.register(Artist, ArtistAdmin)
