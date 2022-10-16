739ca2d02345cdb243321e4dfa324ee0a012b0f9
AbdullahHafez <abdullah.alaa12@gmail.com>
2022-10-16 05:54:41 +0200

    feat: Create a view to list all albums grouped by artists

--- artists/views.py
+++ artists/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Artist
 
 # Create your views here.
+
+def index(request):
+    artists_list = Artist.objects.all()
+    context = {'artists_list': artists_list}
+    return render(request, 'artists/index.html', context)
