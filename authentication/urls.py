9de5be35f60c02dfa1739ee512bdde0fb3167edc
AbdullahHafez <abdullah.alaa12@gmail.com>
2022-10-27 23:07:06 +0200

    feat: Create login & logout endpoints

---
+++ authentication/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from knox import views as knox_views
+from . import views
+
+app_name = 'authentication'
+urlpatterns = [
+    path('register/', views.Register.as_view(), name='register'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='login'),
+]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views import View
+from knox.views import LoginView as KnoxLoginView
+
+from rest_framework.authentication import BasicAuthentication
+from users.serializers import UserSerializer as DefaultUserSerializer
 
 # Create your views here.
+
+
+class Register(View):
+    pass
+
+
+class Login(KnoxLoginView):
+    authentication_classes = [BasicAuthentication, ]
+
+    def get_post_response_data(self, request, token, instance):
+        UserSerializer = DefaultUserSerializer
+
+        data = {
+            'token': token
+        }
+        if UserSerializer is not None:
+            data["user"] = UserSerializer(
+                request.user,
+                context=self.get_context()
+            ).data
+        return data
