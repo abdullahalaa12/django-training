c427fe2df3a7c4aeba6190b3b33becd075e0be38
AbdullahHafez <abdullah.alaa12@gmail.com>
2022-11-01 18:09:28 +0200

    fix: Change /authentication/logout/ path name to logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,5 +7,5 @@ app_name = 'authentication'
 urlpatterns = [
     path('register/', views.Register.as_view(), name='register'),
     path('login/', views.Login.as_view(), name='login'),
-    path('logout/', knox_views.LogoutView.as_view(), name='login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
 ]
