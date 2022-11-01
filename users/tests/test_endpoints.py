e90ea7b7c96d4c9b946bfcdd4e5a39b22ddc3cb5
AbdullahHafez <abdullah.alaa12@gmail.com>
2022-11-01 13:39:27 +0200

    refactor: Make users tests class based

--- users/tests/test_endpoints.py
+++ users/tests/test_endpoints.py
@@ -7,77 +7,55 @@ User = get_user_model()
 
 
 @pytest.mark.django_db
-def test_detail_get_returned_fields(auth_client, user_data_1):
-    url = reverse('users:detail', kwargs={'pk': user_data_1.pk})
-
-    client = auth_client(user_data_1)
-    response = client.get(url)
-
-    assert response.data == {'id': user_data_1.id, 'username': user_data_1.username,
-                             'email': user_data_1.email, 'bio': user_data_1.bio, }
-
-
-@pytest.mark.django_db
-def test_detail_patch_authenticated(auth_client, user_data_1):
-    url = reverse('users:detail', kwargs={'pk': user_data_1.pk})
-
-    bio = 'updated_bio'
-    client = auth_client(user_data_1)
-    response = client.patch(url, {'bio': bio})
-
-    assert response.status_code == 200
-    assert response.data == {'id': user_data_1.id, 'username': user_data_1.username,
-                             'email': user_data_1.email, 'bio': bio}
-
-
-@pytest.mark.django_db
-def test_detail_patch_non_authenticated(auth_client, user_data_1):
-    url = reverse('users:detail', kwargs={'pk': user_data_1.pk})
-
-    bio = 'updated_bio'
-    client = auth_client()
-    response = client.patch(url, {'bio': bio})
-
-    assert response.status_code == 403
-
-
-@pytest.mark.django_db
-def test_detail_put_authenticated(auth_client, user_data_1):
-    url = reverse('users:detail', kwargs={'pk': user_data_1.pk})
-
+class TestUsers:
     username = 'updated_username'
     email = 'updated_email@gmail.com'
     bio = 'updated_bio'
-    client = auth_client(user_data_1)
-    response = client.put(url, {'username': username, 'email': email, 'bio': bio})
 
-    assert response.status_code == 200
-    assert response.data == {'id': user_data_1.id, 'username': username,
-                             'email': email, 'bio': bio}
+    def test_detail_get_returned_fields(self, auth_client, user_data_1):
+        url = reverse('users:detail', kwargs={'pk': user_data_1.pk})
+        client = auth_client(user_data_1)
+        response = client.get(url)
 
+        assert response.data == {'id': user_data_1.id, 'username': user_data_1.username,
+                                 'email': user_data_1.email, 'bio': user_data_1.bio, }
 
-@pytest.mark.django_db
-def test_detail_put_authenticated_missing_fields(auth_client, user_data_1):
-    url = reverse('users:detail', kwargs={'pk': user_data_1.pk})
+    def test_detail_patch_authenticated(self, auth_client, user_data_1):
+        url = reverse('users:detail', kwargs={'pk': user_data_1.pk})
+        client = auth_client(user_data_1)
+        response = client.patch(url, {'bio': self.bio})
 
-    client = auth_client(user_data_1)
-    response = client.put(url)
+        assert response.status_code == 200
+        assert response.data == {'id': user_data_1.id, 'username': user_data_1.username,
+                                 'email': user_data_1.email, 'bio': self.bio}
 
-    expected_error_msg = ['This field is required.']
-    assert response.status_code == 400
-    assert response.data == {'username': expected_error_msg,
-                             'email': expected_error_msg,
-                             'bio': expected_error_msg}
+    def test_detail_patch_non_authenticated(self, auth_client, user_data_1):
+        url = reverse('users:detail', kwargs={'pk': user_data_1.pk})
+        client = auth_client()
+        response = client.patch(url, {'bio': self.bio})
 
+        assert response.status_code == 403
 
-@pytest.mark.django_db
-def test_detail_put_non_authenticated(auth_client, user_data_1):
-    url = reverse('users:detail', kwargs={'pk': user_data_1.pk})
+    def test_detail_put_authenticated(self, auth_client, user_data_1):
+        url = reverse('users:detail', kwargs={'pk': user_data_1.pk})
+        client = auth_client(user_data_1)
+        response = client.put(url, {'username': self.username, 'email': self.email, 'bio': self.bio})
 
-    username = 'updated_username'
-    email = 'updated_email@gmail.com'
-    bio = 'updated_bio'
-    client = auth_client()
-    response = client.put(url, {'username': username, 'email': email, 'bio': bio})
+        assert response.status_code == 200
+        assert response.data == {'id': user_data_1.id, 'username': self.username, 'email': self.email, 'bio': self.bio}
 
-    assert response.status_code == 403
+    def test_detail_put_authenticated_missing_fields(self, auth_client, user_data_1):
+        url = reverse('users:detail', kwargs={'pk': user_data_1.pk})
+        client = auth_client(user_data_1)
+        response = client.put(url)
+        expected_error_msg = ['This field is required.']
+
+        assert response.status_code == 400
+        assert response.data == {'username': expected_error_msg, 'email': expected_error_msg, 'bio': expected_error_msg}
+
+    def test_detail_put_non_authenticated(self, auth_client, user_data_1):
+        url = reverse('users:detail', kwargs={'pk': user_data_1.pk})
+        client = auth_client()
+        response = client.put(url, {'username': self.username, 'email': self.email, 'bio': self.bio})
+
+        assert response.status_code == 403
