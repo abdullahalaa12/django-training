e1316217099ce88098abf591481363a20f538e04
AbdullahHafez <abdullah.alaa12@gmail.com>
2022-11-02 18:00:22 +0200

    feat: Support LimitOffsetPagination with default page limit 10 for /albums/

--- albums/views.py
+++ albums/views.py
@@ -1,11 +1,16 @@
-from rest_framework import generics
+from rest_framework import generics, pagination
 
 from .models import Album
 from .serializers import AlbumSerializer
 from .permissions import IsArtistOrReadOnly
 
 
+class AlbumPagination(pagination.LimitOffsetPagination):
+    default_limit = 10
+
+
 class Index(generics.ListCreateAPIView):
     queryset = Album.approved_albums.all()
     serializer_class = AlbumSerializer
     permission_classes = [IsArtistOrReadOnly, ]
+    pagination_class = AlbumPagination
