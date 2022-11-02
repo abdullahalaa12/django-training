d439f58cf12d4d5e50ebdb4cd084d6ea08c97145
AbdullahHafez <abdullah.alaa12@gmail.com>
2022-11-02 15:10:55 +0200

    feat: Create endpoint /albums/

--- albums/urls.py
+++ albums/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 app_name = 'albums'
-urlpatterns = []
+urlpatterns = [
+    path('', views.Index.as_view(), name='index')
+]
--- albums/views.py
+++ albums/views.py
@@ -1,5 +1,11 @@
-from django.urls import reverse
-from django.views.generic import CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from rest_framework import generics
 
-from .forms import AlbumForm
+from .models import Album
+from .serializers import AlbumSerializer
+from .permissions import IsArtistOrReadOnly
+
+
+class Index(generics.ListCreateAPIView):
+    queryset = Album.approved_albums.all()
+    serializer_class = AlbumSerializer
+    permission_classes = [IsArtistOrReadOnly, ]
